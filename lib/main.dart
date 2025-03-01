ff683d4f91fb2fb3c703cc8b5645ed5e0f0fc449
RukshanRanasinghe2000 <112747422+RukshanRanasinghe2000@users.noreply.github.com>
2025-03-01 22:59:04 +0530

    fix: change title

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'GPA Calculator',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           useMaterial3: true,
